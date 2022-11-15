78ed12d9c3974338648737684d7643da8cfbb714
Diego-Fdez <dieghoxcr7@gmail.com>
2022-11-14 22:42:07 -0600

    tarea05

---
+++ tarea05.js
@@ -0,0 +1,6 @@
+const altura = parseInt(174);
+const alturaMetros = parseFloat(174.7);
+const peso = parseFloat(63.4);
+const alturaRedondeada = Math.ceil(alturaMetros);
+const pesoRedondeado = Math.floor(peso);
+const comparar = Number.MAX_VALUE + 1 === Number.MAX_VALUE;
