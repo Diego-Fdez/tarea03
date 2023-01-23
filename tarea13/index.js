5aa524278664c4f327ae834b8ff0ae860ca58d00
Diego-Fdez <dieghoxcr7@gmail.com>
2023-01-22 19:01:19 -0600

    tarea 13

---
+++ tarea13/index.js
@@ -0,0 +1,7 @@
+const btn = document.querySelector('button');
+
+btn.addEventListener('click', () => alert('click en el botón'));
+
+$(() => {
+  $('button').click(() => console.log('Hola, estoy utilizando jQuery'));
+});
